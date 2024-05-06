fd7f1cf922d7d4f19c0c2bc9b99ae0e8f98cd00a
Deepa Regi <40014553+deepaoctopix@users.noreply.github.com>
2024-05-06 10:27:34 +0530

    advocacy

--- app/src/api/advocacyApi.js
+++ app/src/api/advocacyApi.js
@@ -3,26 +3,26 @@ import { BASE_URL } from '../helper/constant'
 import { setHeaders } from './apiHelpers'
 
 export const listAdvocacy = (body) => {
-  setHeaders()
-  return axios.post(`${BASE_URL}/member/?method=advocacy-list`, body).then((response) => {
-    return response.data
-  })
+    setHeaders()
+    return axios.post(`${BASE_URL}/member/?method=advocacy-list`, body).then((response) => {
+        return response.data
+    })
 }
 
 export const viewAdvocacy = (id, type) => {
-  setHeaders()
-  return axios
-    .get(`${BASE_URL}/member/?method=advocacy-details&id=${id}&type=${type}`)
-    .then((response) => {
-      return response.data
-    })
+    setHeaders()
+    return axios
+        .get(`${BASE_URL}/member/?method=advocacy-details&id=${id}&type=${type}`)
+        .then((response) => {
+            return response.data
+        })
 }
 
 export const takeAction = (body, id) => {
-  setHeaders()
-  return axios
-    .post(`${BASE_URL}/member/?method=advocacy-approve?id=${id}`, body)
-    .then((response) => {
-      return response.data
-    })
+    setHeaders()
+    return axios
+        .post(`${BASE_URL}/member/?method=advocacy-approve&id=${id}`, body)
+        .then((response) => {
+            return response.data
+        })
 }
