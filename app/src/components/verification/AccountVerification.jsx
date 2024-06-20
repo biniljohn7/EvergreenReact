914a2d711bc5f4638c6ae177a013d70c49f05bf8
Albert John <albert@octopix.net>
2024-06-20 18:07:12 +0530

    account verification page

--- app/src/components/verification/AccountVerification.jsx
+++ app/src/components/verification/AccountVerification.jsx
@@ -2,16 +2,23 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BASE_URL } from '../../helper/constant'
 import { useParams } from 'react-router-dom';
+import successImg from '../../assets/images/email-sucess.png';
+import failedImg from '../../assets/images/email-failed.png';
+import {
+  WEBSITE_URL
+} from '../../helper/constant'
 
 const AccountVerification = () => {
   const { t } = useParams();
   const [verificationResult, setVerificationResult] = useState(null);
+  const [verificationSts, setVerificationSts] = useState(null);
 
   useEffect(() => {
     const verifyAccount = async (token) => {
       try {
         const response = await axios.post(`${BASE_URL}/public/?method=account-verify`, { token });
         setVerificationResult(response.data.message);
+        setVerificationSts(response.data.success);
       } catch (error) {
         setVerificationResult('Verification failed');
       }
@@ -26,8 +33,26 @@ const AccountVerification = () => {
 
   return (
     <div>
-      <h1>Account Verification</h1>
-      {verificationResult && <p>{verificationResult}</p>}
+      <div class="verification-box">
+        <div class="img-box">
+          <img src={verificationSts ? successImg : failedImg} />
+        </div>
+        <h1>Account Verification</h1>
+        {verificationResult && <p className='msg'>{verificationResult}</p>}
+
+        {
+          verificationSts ?
+            <div class="text-box">
+              <p>
+                <a href={WEBSITE_URL + 'signin'} className='login-lnk'>
+                  Login to your account
+                </a>
+              </p>
+            </div>
+            :
+            <p></p>
+        }
+      </div>
     </div>
   );
 };
