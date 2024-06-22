b5d3ffb7d0df8a439dfb09cbd6ec24a0a952c3ec
biniljohn7 <39973683+biniljohn7@users.noreply.github.com>
2024-06-22 10:47:17 +0530

    updates

--- app/src/helper/constant.js
+++ app/src/helper/constant.js
@@ -1,9 +1,9 @@
 export const pl = process.env.REACT_APP_ENV;
-// export const BASE_URL = process.env.REACT_APP_API_URL;
+export const BASE_URL = process.env.REACT_APP_API_URL;
 export const ISLOCAL = !!window.localStorage.isLocal;
-export const BASE_URL = 1 ?
-    'http://localhost/evergreenadmin/api' :
-    'https://memberapp-api.apps.openxcell.dev/api';
+// export const BASE_URL = 1 ?
+//     'http://localhost/evergreenadmin/api' :
+//     'https://memberapp-api.apps.openxcell.dev/api';
 
 
 export const SECONDARY_COLOR = "#C33FD9";
