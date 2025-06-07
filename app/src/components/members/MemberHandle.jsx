63c7baea7a45dbc1206c297ce6763bafdbb0d105
Deepa Regi <40014553+deepaoctopix@users.noreply.github.com>
2025-06-07 10:55:30 +0530

    new member hadling page

---
+++ app/src/components/members/MemberHandle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MemberHandle = () => {
+  return (
+    <div className="p-4 rounded-2xl shadow-lg bg-white">
+      <h1 className="text-xl font-bold mb-2">Hello from MyComponent</h1>
+      <p className="text-base text-gray-700">
+        This is a simple React component written in JSX.
+      </p>
+    </div>
+  );
+};
+
+export default MemberHandle;
