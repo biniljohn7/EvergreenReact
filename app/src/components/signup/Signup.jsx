60b3284613a9abb03eb4b92e4d06cb47b5d069e2
Deepa Regi <40014553+deepaoctopix@users.noreply.github.com>
2025-06-24 11:58:37 +0530

    fixes

--- app/src/components/signup/Signup.jsx
+++ app/src/components/signup/Signup.jsx
@@ -3,6 +3,7 @@ import { compose } from "redux";
 import SignUpWrapper from "./signup.style";
 import Button from "../../UI/button/button";
 import Input from "../../UI/input/input";
+import Select from "../../UI/select/select";
 import { withRouter } from "react-router-dom";
 import { Modal } from "reactstrap";
 import {
@@ -16,7 +17,13 @@ import enhancer from "./enhancer";
 import { Link } from "react-router-dom";
 import FB from "../../assets/images/fb_icon_1x.png";
 import Google from "../../assets/images/google_icon_1x.png";
-import { signUp as createAccount, logInViaSMedia } from "../../api/commonAPI";
+import {
+  signUp as createAccount,
+  logInViaSMedia,
+  getSection,
+  getAffiliation,
+  getCollegiateDropdown,
+} from "../../api/commonAPI";
 
 import Toast from "../../UI/Toast/Toast";
 import Spinner from "../../UI/Spinner/Spinner";
@@ -64,6 +71,9 @@ const loadGoogleSDK = () => {
 
 const SignUp = (props) => {
   const [passwordType, setPasswordType] = useState("password");
+  const [sectionList, setSectionList] = useState([]);
+  const [affiliationList, setAffiliationList] = useState([]);
+  const [collegiateSectionList, setCollegiateSectionList] = useState([]);
 
   const Tst = Toast();
   const Spn = Spinner();
@@ -352,19 +362,40 @@ const SignUp = (props) => {
                 />
                 <Error field="email" />
               </div>
+              <div className="fm-row">
+                <Select
+                  label="SECTION"
+                  placeholder="CHOOSE SECTION"
+                  id="section"
+                  options={sectionList}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.section || ""}
+                />
+              </div>
             </div>
             <div className="form-col">
               <div className="fm-row">
-                <Input
-                  label="MEMBER ID"
-                  type="text"
-                  placeholder="MEMBER ID"
-                  id="memberId"
+                <Select
+                  label="AFFILIATION"
+                  placeholder="CHOOSE AFFILIATION"
+                  id="affiliation"
+                  options={affiliationList}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  value={values.memberId || ""}
+                  value={values.affiliation || ""}
                 />
-                <Error field="memberId" />
+              </div>
+              <div className="fm-row">
+                <Select
+                  label="COLLEGIATE SECTION"
+                  placeholder="CHOOSE COLLEGIATE SECTION"
+                  id="collegiateSection"
+                  options={collegiateSectionList}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.collegiateSection || ""}
+                />
               </div>
               <div className="fm-row">
                 <div className="position-relative">
@@ -392,8 +423,12 @@ const SignUp = (props) => {
                       }}
                     ></i>
                   )}
+                  <Error field="password" />
+                  <div className="pws-rule">
+                    Must Contain 8 Characters, One Uppercase, One Lowercase, One
+                    Number and one special case Character
+                  </div>
                 </div>
-                <Error field="password" />
               </div>
               <div className="fm-row">
                 <Input
